f2f1f4cebbf8ceff0735d94f1c0f494b53b37c6d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-21 16:46:07 +0530

    first commit

--- src/main/java/com/bhas/BluePeafowlApplication.java
+++ src/main/java/com/bhas/BluePeafowlApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BluePeafowlApplication {
+public class BluePeafowlApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+        {
 		SpringApplication.run(BluePeafowlApplication.class, args);
+                System.out.println("Indian Peackok,");
 	}
 
 }
