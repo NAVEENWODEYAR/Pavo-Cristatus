db69f8fe9e1e6f4cf4561be62e99d3067aae82e1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-25 00:36:09 +0530

    logOut

--- src/main/java/com/bhas/service/EmployeeService.java
+++ src/main/java/com/bhas/service/EmployeeService.java
@@ -1,10 +1,14 @@
 
 package com.bhas.service;
 
+import com.bhas.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService 
 {
-    
+    // DI 
+    @Autowired
+    private EmployeeRepo employeeRepo;
 }
