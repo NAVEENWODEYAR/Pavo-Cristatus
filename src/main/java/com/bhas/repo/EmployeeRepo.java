a167db452cfd9c7c40f9879755579efeabfc6354
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-25 00:13:17 +0530

    public interface EmployeeRepo extends JpaRepository

---
+++ src/main/java/com/bhas/repo/EmployeeRepo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.bhas.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Repository
+public interface EmployeeRepo extends JpaRepository 
+{
+    
+}
