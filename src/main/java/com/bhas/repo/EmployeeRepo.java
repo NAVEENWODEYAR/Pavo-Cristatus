73dfe9c6c167382b192b4d54c6f6f5151992dfc0
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-28 22:57:11 +0530

    logOut

--- src/main/java/com/bhas/repo/EmployeeRepo.java
+++ src/main/java/com/bhas/repo/EmployeeRepo.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.bhas.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author LENOVO
- */
+
 @Repository
 public interface EmployeeRepo extends JpaRepository 
 {
