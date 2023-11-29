69078f9c016f97264a9d2662ec36f0848b3c8d92
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-29 22:23:37 +0530

    JPARepository

--- src/main/java/com/bhas/repo/EmployeeRepo.java
+++ src/main/java/com/bhas/repo/EmployeeRepo.java
@@ -3,10 +3,11 @@ package com.bhas.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.bhas.entity.*;
 
 
 @Repository
-public interface EmployeeRepo extends JpaRepository 
+public interface EmployeeRepo extends JpaRepository <Employee,Integer>
 {
     
 }
